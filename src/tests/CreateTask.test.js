21e3460ee647bddcc6b6b39acb803f1fc6259265
Amin Chouaibi el Azaar <achouaibi@plainconcepts.com>
2021-06-02 19:20:46 +0200

    Sync

---
+++ src/tests/CreateTask.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import { CreateTask } from "../components/CreateTask";
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+
+test("render create task component renders static content", () => {
+  const expectedContent = "Name of Worker";
+  const component = render(<CreateTask taskCreated={mockedHandler} />);
+  const input = component.getByTestId("AssignedTo");
+  expect(input.placeholder).toEqual(expectedContent);
+});
