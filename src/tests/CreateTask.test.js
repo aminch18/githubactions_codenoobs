33e9bb700ee38a68d319aecd4c2bc0882cbb26d0
Amin Chouaibi el Azaar <achouaibi@plainconcepts.com>
2021-06-03 19:23:52 +0200

    Cleaning

--- src/tests/CreateTask.test.js
+++ src/tests/CreateTask.test.js
@@ -4,7 +4,8 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 
 test("render create task component renders static content", () => {
-  const expectedContent = "Name of Worker";
+  const expectedContent = "Name of worker";
+  let mockedHandler = jest.fn(() => true);
   const component = render(<CreateTask taskCreated={mockedHandler} />);
   const input = component.getByTestId("AssignedTo");
   expect(input.placeholder).toEqual(expectedContent);
